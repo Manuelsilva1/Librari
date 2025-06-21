32d2f44d485abaf997f402a5aa3b7b128993efd3
Manuel Silva <tuemail@example.com>
2025-06-20 21:05:51 -0300

    Cambio

--- libreria/src/main/java/com/api/libreria/security/CustomUserDetailsService.java
+++ libreria/src/main/java/com/api/libreria/security/CustomUserDetailsService.java
@@ -17,9 +17,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) {
-        System.out.println("username = " + username);
-        User user = userRepository.findByUsername(username)
+    public UserDetails loadUserByUsername(String email) {
+        System.out.println("username = " + email);
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
 
         log.info("user: " + user);
